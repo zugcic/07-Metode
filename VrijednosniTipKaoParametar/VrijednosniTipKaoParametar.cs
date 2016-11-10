ac9897226daab761f29fc9b95aed2039310b7dd9
Stjepan Zugcic <stzugcic@vsite.hr>
2016-11-10 21:24:57 +0100

    Riješili smo VrijednosniTipKaoParametar

--- VrijednosniTipKaoParametar/VrijednosniTipKaoParametar.cs
+++ VrijednosniTipKaoParametar/VrijednosniTipKaoParametar.cs
@@ -59,12 +59,14 @@ namespace Vsite.CSharp
 
             // TODO: Ispod sljedeće naredbe dodati poziv metode PromijeniOsobu s prosljeđenom osobom janko i ispisati osobu nakon poziva te metode.
             Osoba janko = new Osoba("Janko", 1);
-
+			PromijeniOsobu(janko);
+			Console.WriteLine(janko);
 
             Console.WriteLine();
 
-            // TODO: Napisati poziv metode PromijeniIme s prosljeđenom osobom janko i novim imenom "Pero" i ispisati osobu nakon poziva te metode.
-            
+			// TODO: Napisati poziv metode PromijeniIme s prosljeđenom osobom janko i novim imenom "Pero" i ispisati osobu nakon poziva te metode.
+			PromijeniIme(janko, "Pero");
+			Console.WriteLine(janko);
 
 
             Console.WriteLine("GOTOVO!!!");
