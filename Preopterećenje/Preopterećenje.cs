172964c1d979de87ba640c48e94866d6eff168fe
Stjepan Zugcic <stzugcic@vsite.hr>
2016-11-10 21:09:59 +0100

    Riješili smo Preopterećenje()

--- Preopterećenje/Preopterećenje.cs
+++ Preopterećenje/Preopterećenje.cs
@@ -1,22 +1,25 @@
 ﻿using System;
-
+  //preopterećenje metoda overload -> isto ime metode, a različiti parametri
 namespace Vsite.CSharp
 {
     public class Preopterećenje
     {
-        // TODO: Pokrenuti program i provjeriti ispis.
+        //  Pokrenuti program i provjeriti ispis.
         public static void MojaMetoda(double broj1, double broj2)
         {
             Console.WriteLine("MojaMetoda(double {0}, double {1})", broj1, broj2);
         }
 
-        // TODO: Napisati statičku javno datupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati "MojaMetoda(int {0}, double {1})", broj1, broj2
+		//  Napisati statičku javno datupnu preopterećenu verziju metode MojaMetoda koja prima dva argumenta: prvi tipa int, a drugi tipa double. Metoda mora ispisati "MojaMetoda(int {0}, double {1})", broj1, broj2
+		public static void MojaMetoda(int broj1, double broj2)
+		{
+			Console.WriteLine("MojaMetoda(int {0}, double {1})", broj1, broj2);
+		}
+
+		//  Pokrenuti program i usporediti ispis sa prijašnjim.
 
 
-        // TODO: Pokrenuti program i usporediti ispis sa prijašnjim.
-
-
-        static void Main(string[] args)
+		static void Main(string[] args)
         {
             int i1 = 1;
             int i2 = 2;
